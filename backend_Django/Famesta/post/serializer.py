f8653f873ecb15f46c8b78822e7062207a856144
vipin-s0106 <vipin.s0106@gmail.com>
2020-04-26 22:33:41 +0530

    STORY API Completed

--- backend_Django/Famesta/post/serializer.py
+++ backend_Django/Famesta/post/serializer.py
@@ -9,17 +9,33 @@ class PostSerializer(serializers.ModelSerializer):
         model = Post
         fields = '__all__'
 
-
+#Below Serializer is the combination of CustomField and Model Feilds
 class PostListSerializer(serializers.ModelSerializer):
-    # total_comments = serializers.IntegerField()
-    # liked_by = serializers.ListField()
+    total_comment = serializers.SerializerMethodField('get_total_comment')
+    liked_by = serializers.SerializerMethodField('get_liked_by')
 
     class Meta:
         model = Post
-        fields = ['id', 'file', 'post_info', 'post_time_stamp'
-                  # 'total_comments',
-                  # 'liked_by'
-                  ]
+        fields = ['id', 'file', 'post_info', 'post_time_stamp','total_comment','liked_by']
+
+    def get_total_comment(self,obj):
+        if obj:
+            post = Post.objects.filter(id = obj.id).first()
+            list_comments = PostDetail.objects.filter(post=post,comment__isnull=False)
+            return len(list_comments)
+        return None
+
+    def get_liked_by(self,obj):
+        if obj:
+            post = Post.objects.filter(id = obj.id).first()
+            list_liked_post = PostDetail.objects.filter(post = post,like=True)
+            user_liked_post_list = []
+            for comment_post in list_liked_post:
+                user_liked_post_list.append(comment_post.user.username)
+            return user_liked_post_list
+        return None
+
+
 
 
 class PostCommentSerializer(serializers.ModelSerializer):
@@ -34,3 +50,6 @@ class PostDetailSerializer(serializers.ModelSerializer):
     class Meta:
         model = Post
         fields = ['id', 'file', 'post_info', 'post_time_stamp', 'post_detail']
+
+
+
