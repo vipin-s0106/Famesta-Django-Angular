2001bc0f5a2b7c66fc6b3a5d5ba66d2ce44052ed
vipin-s0106 <vipin.s0106@gmail.com>
2020-05-01 14:05:48 +0530

    Angular Part Started

--- frontend_Angular/famesta-fe/src/app/app-routing.module.ts
+++ frontend_Angular/famesta-fe/src/app/app-routing.module.ts
@@ -1,11 +1,26 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component'
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '',redirectTo:'/login',pathMatch:'full'},
+  {path:'login',component:LoginComponent},
+  {path: 'register',component:RegisterComponent},
+  {path:'dashboard',component:DashboardComponent},
+  {path:'**',component:PageNotFoundComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+export const RoutingComponent = [
+  LoginComponent,
+  RegisterComponent,
+  DashboardComponent,
+  PageNotFoundComponent,
+]
--- frontend_Angular/famesta-fe/src/app/app.module.ts
+++ frontend_Angular/famesta-fe/src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { HttpClientModule } from '@angular/common/http';
+
+//Importing RoutingComponenet so Normal Componenet automatically imported
+import { RoutingComponent } from './app-routing.module';
+
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RoutingComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
