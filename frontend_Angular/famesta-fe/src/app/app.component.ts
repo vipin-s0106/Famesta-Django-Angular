b4d9cef1eb8047776c1ef157ec68472695b30c77
vipin-s0106 <vipin.s0106@gmail.com>
2020-06-03 22:52:36 +0530

    notificaion frontend changes

--- frontend_Angular/famesta-fe/src/app/app.component.ts
+++ frontend_Angular/famesta-fe/src/app/app.component.ts
@@ -3,6 +3,8 @@ import { Router, ParamMap } from '@angular/router'
 import { ActivatedRoute } from '@angular/router';
 import { AuthService } from './services/auth.service';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { UserService } from './services/user.service';
+import { NotificationService } from './services/notification.service';
 
 
 
@@ -15,7 +17,25 @@ import { DashboardComponent } from './dashboard/dashboard.component';
 })
 export class AppComponent {
 
-  constructor(public authSrv: AuthService,private route:ActivatedRoute,private router: Router){
+  public notification_count;
+  public logged_user_id;
+
+
+  constructor(public usr_srv: UserService,public not_srv: NotificationService,public authSrv: AuthService,private route:ActivatedRoute,private router: Router){
+    // if(this.authSrv.loggedIn()){
+    //   this.usr_srv.getLoggedUserDetails().subscribe(
+    //     res => {
+    //       this.logged_user_id = res.id;
+    //       this.not_srv.getNotificationCount(res.id).subscribe(
+    //         res => {
+    //           this.notification_count = res;
+    //           this.router.navigate['/dashboard']
+    //         },
+    //         err => this.router.navigate(['/bad-request'])
+    //       )
+    //     }
+    //   )
+    // }
   }
 
 
